a7d18fc7e82846f802fc6ae4330f99bc95dd62e2
Myles Horton <myles@getlantern.org>
2015-07-22 09:15:29 -0700

    very rough first pass

---
+++ quickstart.go
@@ -0,0 +1,159 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"os/user"
+	"path/filepath"
+	"sync"
+
+	"golang.org/x/net/context"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/gmail/v1"
+)
+
+func readAndRespond(user string, srv *gmail.Service, m *gmail.Message, wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Printf("- %v\n", m.Id)
+	msg, e := srv.Users.Messages.Get(user, m.Id).Format("metadata").Do()
+	if e != nil {
+		log.Fatalf("Could not get message! %v", e)
+	} else {
+		for _, h := range msg.Payload.Headers {
+			if h.Name == "Sender" {
+				fmt.Printf("- %v:%v\n", h.Name, h.Value)
+			}
+		}
+	}
+}
+
+// getClient uses a Context and Config to retrieve a Token
+// then generate a Client. It returns the generated Client.
+func getClient(ctx context.Context, config *oauth2.Config) *http.Client {
+	cacheFile, err := tokenCacheFile()
+	if err != nil {
+		log.Fatalf("Unable to get path to cached credential file. %v", err)
+	}
+	tok, err := tokenFromFile(cacheFile)
+	if err != nil {
+		tok = getTokenFromWeb(config)
+		saveToken(cacheFile, tok)
+	}
+	return config.Client(ctx, tok)
+}
+
+// getTokenFromWeb uses Config to request a Token.
+// It returns the retrieved Token.
+func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
+	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+	fmt.Printf("Go to the following link in your browser then type the "+
+		"authorization code: \n%v\n", authURL)
+
+	var code string
+	if _, err := fmt.Scan(&code); err != nil {
+		log.Fatalf("Unable to read authorization code %v", err)
+	}
+
+	tok, err := config.Exchange(oauth2.NoContext, code)
+	if err != nil {
+		log.Fatalf("Unable to retrieve token from web %v", err)
+	}
+	return tok
+}
+
+// tokenCacheFile generates credential file path/filename.
+// It returns the generated credential path/filename.
+func tokenCacheFile() (string, error) {
+	usr, err := user.Current()
+	if err != nil {
+		return "", err
+	}
+	tokenCacheDir := filepath.Join(usr.HomeDir, ".credentials")
+	os.MkdirAll(tokenCacheDir, 0700)
+	return filepath.Join(tokenCacheDir,
+		url.QueryEscape("gmail-api-quickstart.json")), err
+}
+
+// tokenFromFile retrieves a Token from a given file path.
+// It returns the retrieved Token and any read error encountered.
+func tokenFromFile(file string) (*oauth2.Token, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	t := &oauth2.Token{}
+	err = json.NewDecoder(f).Decode(t)
+	defer f.Close()
+	return t, err
+}
+
+// saveToken uses a file path to create a file and store the
+// token in it.
+func saveToken(file string, token *oauth2.Token) {
+	fmt.Printf("Saving credential file to: %s\n", file)
+	f, err := os.Create(file)
+	if err != nil {
+		log.Fatalf("Unable to cache oauth token: %v", err)
+	}
+	defer f.Close()
+	json.NewEncoder(f).Encode(token)
+}
+
+func main() {
+	ctx := context.Background()
+
+	b, err := ioutil.ReadFile("client_secret.json")
+	if err != nil {
+		log.Fatalf("Unable to read client secret file: %v", err)
+	}
+
+	config, err := google.ConfigFromJSON(b, gmail.GmailReadonlyScope)
+	if err != nil {
+		log.Fatalf("Unable to parse client secret file to config: %v", err)
+	}
+	client := getClient(ctx, config)
+
+	srv, err := gmail.New(client)
+	if err != nil {
+		log.Fatalf("Unable to retrieve gmail Client %v", err)
+	}
+
+	user := "me"
+	r, err := srv.Users.Labels.List(user).Do()
+	if err != nil {
+		log.Fatalf("Unable to retrieve labels. %v", err)
+	}
+	if len(r.Labels) > 0 {
+		fmt.Print("Labels:\n")
+		for _, l := range r.Labels {
+			fmt.Printf("- %s\n", l.Name)
+		}
+	} else {
+		fmt.Print("No labels found.")
+	}
+
+	msgsCall := srv.Users.Messages.List(user)
+	msgsCall.MaxResults(1)
+	msgs, er := msgsCall.Do()
+	if er != nil {
+		log.Fatalf("Unable to retrieve messages. %v", er)
+	}
+
+	if len(msgs.Messages) > 0 {
+		var wg sync.WaitGroup
+		fmt.Print("Messages:\n")
+		for _, m := range msgs.Messages {
+			wg.Add(1)
+			go readAndRespond(user, srv, m, &wg)
+		}
+		wg.Wait()
+	} else {
+		fmt.Print("No messages found.")
+	}
+}
